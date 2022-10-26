d8fe507d7bc2f40f2144dbb2d9ae17ff25b0a26a
Talha0009 <talha.abacus@gmail.com>
2022-10-26 13:36:34 +0500

    update authorization

--- HCM.API/HCMModels/VMUserAuthorization.cs
+++ HCM.API/HCMModels/VMUserAuthorization.cs
@@ -2,6 +2,10 @@
 {
     public class VMUserAuthorization
     {
+        public int ID { get; set; }
+        public string CreatedBy { get; set; }
+        public string CreatedDate { get; set; }
+        public string MenuLink { get; set; }
         public int PMenuID { get; set; }
         public string PMenuName { get; set; }
         public int CMenuID { get; set; }
--- HCM.API/Repository/Authorization/UserAuthorizationRepo.cs
+++ HCM.API/Repository/Authorization/UserAuthorizationRepo.cs
@@ -16,53 +16,6 @@ namespace HCM.API.Repository.Authorization
             _DBContext = DBContext;
         }
 
-        public async Task<List<UserAuthorization>> FetchUserAuth(string userID)
-        {
-            List<UserAuthorization> oList = new List<UserAuthorization>();
-            try
-            {
-                await Task.Run(() =>
-                {
-                    if (userID == "manager")
-                    {
-                        string qry1 = @"SELECT  
-                                    ID,
-                                    MenuName,
-                                    MenuLink,
-                                    MenuParent,
-                                    ID as FKMenuID,
-                                    'manager' as FKUserID,
-                                    2 AS UserRights
-                                    ,isnull(CreatedBy,'-') as CreatedBy,isnull(CreatedDate,GetDate()) as CreatedDate,isnull(CAppStamp,'-') as CAppStamp,isnull(UpdatedBy,'-') as UpdatedBy,isnull(UpdatedDate,GetDate()) as UpdatedDate,isnull(UAppStamp,'-') as UAppStamp
-                                    FROM MstMenu ";
-                        oList = _DBContext.UserAuthorizations.FromSqlRaw(qry1).ToList();
-                    }
-                    else
-                    {
-                        string qry = $@"SELECT  
-                                t1.ID,
-                                T1.MenuName,
-                                T1.MenuLink,
-                                T1.MenuParent,
-                                T2.FKMenuID,
-                                T2.FKUserID,
-                                isnull(T2.UserRights,1) AS UserRights
-                                ,isnull(t2.CreatedBy,'-') as CreatedBy,isnull(t2.CreatedDate,GETDATE()) as CreatedDate,isnull(t2.CAppStamp,'-') as CAppStamp,isnull(t2.UpdatedBy,'-') as UpdatedBy,isnull(t2.UpdatedDate,GETDATE()) as UpdatedDate,isnull(t2.UAppStamp,'-') as UAppStamp
-                                FROM MstMenu T1
-                                LEFT JOIN UserAuthorization T2 ON T2.FKMenuID = T1.MenuID and T2.FKUserID = @userID";
-                        oList = _DBContext.UserAuthorizations.FromSqlRaw(qry, new SqlParameter("@userID", userID)).ToList();
-                    }
-
-                });
-            }
-            catch (Exception ex)
-            {
-                Logs.GenerateLogs(ex);
-
-            }
-            return oList;
-        }
-
         public async Task<ApiResponseModel> AddUserAuthorization(List<UserAuthorization> oUserAuthorization)
         {
             ApiResponseModel response = new ApiResponseModel();
@@ -91,14 +44,21 @@ namespace HCM.API.Repository.Authorization
             return response;
         }
 
-        public async Task<List<VMUserAuthorization>> GetAllMenu(string UserID)
+        public async Task<List<VMUserAuthorization>> GetAllAuthorizationMenu(string UserID)
         {
             List<VMUserAuthorization> oList = new List<VMUserAuthorization>();
             try
             {
                 using (var command = _DBContext.Database.GetDbConnection().CreateCommand())
                 {
-                    command.CommandText = $"SELECT b.MenuID AS PMenuID,b.MenuName AS PMenuName\r\n,a.MenuID AS CMenuID,a.MenuName AS CMenuName\r\n,case when isnull(UserRights,1) = 1 then 'false' else 'true' end as UserRights FROM MstMenu a\r\ninner join MstMenu b on a.MenuParent=b.MenuID\r\nleft join UserAuthorization t3 on t3.FKMenuID=a.MenuID and FkUserID = '{UserID}'";
+                    if (UserID == "manager")
+                    {
+                        command.CommandText = $"SELECT isnull(t3.ID,0) as ID,t3.CreatedBy,t3.CreatedDate,a.MenuLink,b.MenuID AS PMenuID,b.MenuName AS PMenuName\r\n,a.MenuID AS CMenuID,a.MenuName AS CMenuName\r\n,case when isnull(UserRights,2) = 1 then 'false' else 'true' end as UserRights FROM MstMenu a\r\ninner join MstMenu b on a.MenuParent=b.MenuID\r\nleft join UserAuthorization t3 on t3.FKMenuID=a.MenuID and FkUserID = '{UserID}'";
+                    }
+                    else
+                    {
+                        command.CommandText = $"SELECT isnull(t3.ID,0) as ID,t3.CreatedBy,t3.CreatedDate,a.MenuLink,b.MenuID AS PMenuID,b.MenuName AS PMenuName\r\n,a.MenuID AS CMenuID,a.MenuName AS CMenuName\r\n,case when isnull(UserRights,1) = 1 then 'false' else 'true' end as UserRights FROM MstMenu a\r\ninner join MstMenu b on a.MenuParent=b.MenuID\r\nleft join UserAuthorization t3 on t3.FKMenuID=a.MenuID and FkUserID = '{UserID}'";
+                    }
                     command.CommandType = CommandType.Text;
                     _DBContext.Database.OpenConnection();
 
@@ -107,6 +67,10 @@ namespace HCM.API.Repository.Authorization
                         while (result.Read())
                         {
                             VMUserAuthorization oData = new VMUserAuthorization();
+                            oData.ID = Convert.ToInt32(result["ID"].ToString());
+                            oData.CreatedBy = result["CreatedBy"].ToString();
+                            oData.CreatedDate = result["CreatedDate"].ToString();
+                            oData.MenuLink = result["MenuLink"].ToString();
                             oData.PMenuID = Convert.ToInt32(result["PMenuID"].ToString());
                             oData.PMenuName = result["PMenuName"].ToString();
                             oData.CMenuID = Convert.ToInt32(result["CMenuID"].ToString());
